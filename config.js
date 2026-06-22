cf68546f9c395a18e925398b690506245f0c16c9
troymd123 <troymd123@gmail.com>
2026-06-21 23:27:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // After you deploy the Worker, replace this with the URL wrangler gives you.
 // It will look like: https://alltroysfault-api.<your-subdomain>.workers.dev
 const CONFIG = {
-  API_BASE: "alltroysfault-api.troymd123.workers.dev",
+  API_BASE: "https://alltroysfault-api.troymd123.workers.dev",
 };
